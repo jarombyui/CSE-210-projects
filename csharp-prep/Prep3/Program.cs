9552ca9ed4d096d79c095c28144118f6c9291137
jarombyui <mcamposro93@ucvvirtual.edu.pe>
2024-09-16 23:42:56 -0500

    solution prep-3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            // Generate a random magic number between 1 and 100
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = 0;
+            int numberOfGuesses = 0;
+
+            // Loop until the user guesses the correct number
+            while (guess != magicNumber)
+            {
+                // Ask the user for their guess
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                // Determine if the guess is higher, lower, or correct
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numberOfGuesses} guesses.");
+                }
+            }
+
+            // Ask the user if they want to play again
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string response = Console.ReadLine().ToLower();
+            playAgain = (response == "yes");
+        }
     }
 }
