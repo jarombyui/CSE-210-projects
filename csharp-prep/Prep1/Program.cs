c235344ab386398e4faa05cb547563e22e2d6afb
jarombyui <mcamposro93@ucvvirtual.edu.pe>
2024-09-16 22:46:48 -0500

    solution prep-1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = System.Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = System.Console.ReadLine();
+        System.Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
